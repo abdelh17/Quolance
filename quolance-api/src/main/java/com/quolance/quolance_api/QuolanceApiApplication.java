7438857db065b4acd9855f2d615245f5fc40ec83
Abdel <abdelkader.habel_pro@outlook.com>
2024-10-15 15:58:05 -0400

    backend project setup #2 #3

---
+++ quolance-api/src/main/java/com/quolance/quolance_api/QuolanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.quolance.quolance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuolanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuolanceApiApplication.class, args);
+	}
+
+}
---
+++ quolance-api/src/test/java/com/quolance/quolance_api/QuolanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quolance.quolance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuolanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
