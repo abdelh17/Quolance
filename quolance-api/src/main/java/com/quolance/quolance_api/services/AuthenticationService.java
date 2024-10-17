8147851a0a008089b84f12c449b2738c773894f8
Abdel <abdelkader.habel_pro@outlook.com>
2024-10-17 17:25:25 -0400

    feat: added refresh token endpoint #3

--- quolance-api/src/main/java/com/quolance/quolance_api/services/AuthenticationService.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/services/AuthenticationService.java
@@ -3,10 +3,16 @@ package com.quolance.quolance_api.services;
 import com.quolance.quolance_api.dtos.LoginResponseDto;
 import com.quolance.quolance_api.dtos.UserLoginDto;
 import com.quolance.quolance_api.dtos.UserRegistrationDto;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthenticationService {
-    UserRegistrationDto signup(UserRegistrationDto input);
 
     LoginResponseDto authenticate(UserLoginDto userLoginDto, HttpServletResponse httpServletResponse);
+
+    void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException, java.io.IOException;
+
+    UserRegistrationDto signup(UserRegistrationDto input);
+
 }
--- quolance-api/src/main/java/com/quolance/quolance_api/services/auth/AuthenticationServiceImpl.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/services/auth/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.quolance.quolance_api.services.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quolance.quolance_api.dtos.LoginResponseDto;
 import com.quolance.quolance_api.dtos.UserDto;
 import com.quolance.quolance_api.dtos.UserLoginDto;
@@ -8,6 +9,9 @@ import com.quolance.quolance_api.entities.User;
 import com.quolance.quolance_api.entities.enums.Role;
 import com.quolance.quolance_api.services.AuthenticationService;
 import com.quolance.quolance_api.services.UserService;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -58,4 +62,46 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         loginResponseDto.setUser(UserDto.fromEntity(authenticatedUser));
         return loginResponseDto;
     }
+
+
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException, java.io.IOException {
+        Cookie[] cookies = request.getCookies();
+
+        String refreshToken = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("refreshToken")) {
+                    refreshToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (refreshToken == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String userEmail = jwtService.extractUsername(refreshToken);
+
+        User user = userService.findByEmail(userEmail).orElseThrow();
+
+        String accessToken = jwtService.generateToken(user, false);
+        String newRefreshToken = jwtService.generateToken(user, true);
+
+        jwtService.addTokensAsCookies(response, accessToken, newRefreshToken);
+
+        LoginResponseDto loginResponseDto = new LoginResponseDto();
+        loginResponseDto.setAccessToken(accessToken);
+        loginResponseDto.setRefreshToken(refreshToken);
+        loginResponseDto.setExpiresIn(jwtService.getExpirationTime());
+        loginResponseDto.setUser(UserDto.fromEntity(user));
+
+        new ObjectMapper().writeValue(response.getOutputStream(), loginResponseDto);
+    }
+
+
+
+
+
 }
