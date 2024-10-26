57f94475584b8643aedb553cbc73980790ff11f7
francesco <francescoferrato@hotmail.com>
2024-10-25 21:52:06 -0400

    Added UI for freelancers to view and filter through projects

---
+++ quolance-ui/src/components/global/BreadCrumb.tsx
@@ -0,0 +1,51 @@
+
+import Link from "next/link";
+import { PiCaretDoubleRight } from "react-icons/pi";
+
+
+type PropsType = {
+  pageName: string;
+  isSearchBoxShow?: boolean;
+  isMiddlePage?: boolean;
+  middlePageName?: string;
+  middlePageLink?: string;
+};
+
+function BreadCrumb({
+  pageName,
+  isMiddlePage,
+  middlePageName,
+  middlePageLink,
+}: PropsType) {
+  return (
+    <>
+      <section className=" bg-n20 p-6">
+        <div className="container">
+          <ul className="flex items-center justify-start gap-2 pt-3 font-medium">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            {isMiddlePage && (
+              <>
+                <li className="">
+                  <PiCaretDoubleRight />
+                </li>
+                <li>
+                  <Link href={middlePageLink!}>{middlePageName}</Link>
+                </li>
+              </>
+            )}
+            <li className="text-r300">
+              <PiCaretDoubleRight />
+            </li>
+            <li className="text-r300">{pageName}</li>
+          </ul>
+         
+        </div>
+      </section>
+
+    </>
+  );
+}
+
+export default BreadCrumb;
