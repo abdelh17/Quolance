fe94c2d1cbe6497d7f53f05c990a7ba8798a82bf
NotMalek <malekanes213@gmail.com>
2024-10-26 16:06:13 -0400

    #27 #7 Created endpoints for get all and get by id

--- quolance-api/src/main/java/com/quolance/quolance_api/controllers/FreelancerController.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/controllers/FreelancerController.java
@@ -1,7 +1,9 @@
 package com.quolance.quolance_api.controllers;
 
 import com.quolance.quolance_api.dtos.ApplicationDto;
+import com.quolance.quolance_api.dtos.ProjectDto;
 import com.quolance.quolance_api.services.FreelancerService;
+import com.quolance.quolance_api.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FreelancerController {
     private final FreelancerService freelancerService;
+    private final ProjectService projectService;
 
     @PostMapping("/submit-application")
     public ResponseEntity<ApplicationDto> createProject(@RequestBody ApplicationDto applicationDto) {
@@ -24,6 +27,18 @@ public class FreelancerController {
     public ResponseEntity<List<ApplicationDto>> getAllMyApplications(@PathVariable(name = "id") Long freelancerId) {
         List<ApplicationDto> applications = freelancerService.getMyApplications(freelancerId);
         return ResponseEntity.ok(applications);
+    }
+
+    @GetMapping("/projects")
+    public ResponseEntity<List<ProjectDto>> getAllProjects() {
+        List<ProjectDto> projects = projectService.getAllProjects();
+        return ResponseEntity.ok(projects);
+    }
+
+    @GetMapping("/projects/{id}")
+    public ResponseEntity<ProjectDto> getProjectById(@PathVariable Long id) {
+        ProjectDto project = projectService.getProjectById(id);
+        return ResponseEntity.ok(project);
     }
 }
 
