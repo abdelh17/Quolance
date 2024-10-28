4d22382047da6d9da631245e9e95d7aa9a35a25f
Abdel <abdelkader.habel_pro@outlook.com>
2024-10-28 17:54:18 -0400

    documentation: updated postman collection and documented the api for swagger

--- quolance-api/src/main/java/com/quolance/quolance_api/controllers/ClientController.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/controllers/ClientController.java
@@ -4,6 +4,7 @@ import com.quolance.quolance_api.dtos.ApplicationDto;
 import com.quolance.quolance_api.dtos.ProjectDto;
 import com.quolance.quolance_api.services.ApplicationService;
 import com.quolance.quolance_api.services.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +19,31 @@ public class ClientController {
     private final ApplicationService applicationService;
 
     @PostMapping("/create-project")
+    @Operation(
+            summary = "Create a project",
+            description = "Create a project by passing a project dto"
+    )
     public ResponseEntity<ProjectDto> createProject(@RequestBody ProjectDto projectDto) {
         ProjectDto project = clientService.createProject(projectDto);
         return ResponseEntity.ok(project);
     }
 
-    @GetMapping("/client/{id}/projects")
-    public ResponseEntity<List<ProjectDto>> getAllMyProjects(@PathVariable(name = "id") Long clientId) {
+    @GetMapping("/{clientId}/projects")
+    @Operation(
+            summary = "Get all projects of a client",
+            description = "Get all projects of a client by passing the client ID"
+    )
+    public ResponseEntity<List<ProjectDto>> getAllMyProjects(@PathVariable(name = "clientId") Long clientId) {
         List<ProjectDto> projects = clientService.getMyProjects(clientId);
         return ResponseEntity.ok(projects);
     }
 
-    @GetMapping("/projects/{id}/applications")
-    public ResponseEntity<List<ApplicationDto>> getAllApplicationsToProject(@PathVariable(name = "id") Long projectId) {
+    @GetMapping("/projects/{projectId}/applications")
+    @Operation(
+            summary = "Get all applications on a project",
+            description = "Get all applications on a project by passing the project ID"
+    )
+    public ResponseEntity<List<ApplicationDto>> getAllApplicationsToProject(@PathVariable(name = "projectId") Long projectId) {
         List<ApplicationDto> applications = applicationService.getApplicationsByProjectId(projectId);
         return ResponseEntity.ok(applications);
     }
