350388c7f7272fb66279f8a32718aeb53880e5a2
Zakaria <zackelmaoo@hotmail.fr>
2024-10-27 21:00:14 -0400

    A client can see applications to a specific project. Contains bugfix for #45. Closes #28. Parent: #6

--- quolance-api/src/main/java/com/quolance/quolance_api/controllers/ClientController.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/controllers/ClientController.java
@@ -1,6 +1,8 @@
 package com.quolance.quolance_api.controllers;
 
+import com.quolance.quolance_api.dtos.ApplicationDto;
 import com.quolance.quolance_api.dtos.ProjectDto;
+import com.quolance.quolance_api.services.ApplicationService;
 import com.quolance.quolance_api.services.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ClientController {
     private final ClientService clientService;
+    private final ApplicationService applicationService;
 
     @PostMapping("/create-project")
     public ResponseEntity<ProjectDto> createProject(@RequestBody ProjectDto projectDto) {
@@ -20,9 +23,15 @@ public class ClientController {
         return ResponseEntity.ok(project);
     }
 
-    @GetMapping("/projects/client/{id}")
+    @GetMapping("/client/{id}/projects")
     public ResponseEntity<List<ProjectDto>> getAllMyProjects(@PathVariable(name = "id") Long clientId) {
         List<ProjectDto> projects = clientService.getMyProjects(clientId);
         return ResponseEntity.ok(projects);
+    }
+
+    @GetMapping("/projects/{id}/applications")
+    public ResponseEntity<List<ApplicationDto>> getAllApplicationsToProject(@PathVariable(name = "id") Long projectId) {
+        List<ApplicationDto> applications = applicationService.getApplicationsByProjectId(projectId);
+        return ResponseEntity.ok(applications);
     }
 }
--- quolance-api/src/main/java/com/quolance/quolance_api/services/impl/ClientServiceImpl.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/services/impl/ClientServiceImpl.java
@@ -1,8 +1,10 @@
 package com.quolance.quolance_api.services.impl;
 
+import com.quolance.quolance_api.dtos.ApplicationDto;
 import com.quolance.quolance_api.dtos.ProjectDto;
 import com.quolance.quolance_api.entities.Project;
 import com.quolance.quolance_api.entities.User;
+import com.quolance.quolance_api.services.ApplicationService;
 import com.quolance.quolance_api.services.ClientService;
 import com.quolance.quolance_api.services.ProjectService;
 import com.quolance.quolance_api.services.UserService;
@@ -16,6 +18,7 @@ import java.util.List;
 public class ClientServiceImpl implements ClientService {
     private final ProjectService projectService;
     private final UserService userService;
+    private final ApplicationService applicationService;
 
     @Override
     public ProjectDto createProject(ProjectDto projectDto) {
@@ -30,5 +33,11 @@ public class ClientServiceImpl implements ClientService {
     public List<ProjectDto> getMyProjects(Long clientId) {
         List<ProjectDto> projects = projectService.getProjectsByClientId(clientId);
         return projects;
+    }
+
+    @Override
+    public List<ApplicationDto> getApplicationsToMyProject(Long projectId) {
+        List<ApplicationDto> applications = applicationService.getApplicationsByProjectId(projectId);
+        return applications;
     }
 }
