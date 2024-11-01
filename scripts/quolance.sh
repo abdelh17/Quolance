7e9222ce92c9b3ab6172b54dab32c301abe90ac3
Zakaria <zackelmaoo@hotmail.fr>
2024-10-31 21:20:27 -0400

    quolance internal script

--- scripts/quolance.sh
+++ scripts/quolance.sh
@@ -14,7 +14,7 @@ cursor=0
 header() {
   clear
   echo "============================"
-  echo "  Quolance Internal Script  "
+  echo "   Quolance Internal Script  "
   echo "============================"
   echo
   echo "Choose an option for the Docker Compose container:"
@@ -34,53 +34,52 @@ print_menu() {
   done
 }
 
-# Handle key inputs
+# Function to handle key inputs
+move_cursor() {
+  read -rsn1 key
+  if [[ $key == $'\x1b' ]]; then
+    read -rsn2 -t 0.1 key
+    if [[ $key == "[A" ]]; then
+      # Up arrow
+      ((cursor--))
+      if [ "$cursor" -lt 0 ]; then
+        cursor=$((${#options[@]} - 1))
+      fi
+    elif [[ $key == "[B" ]]; then
+      # Down arrow
+      ((cursor++))
+      if [ "$cursor" -ge "${#options[@]}" ]; then
+        cursor=0
+      fi
+    fi
+  elif [[ $key == "" ]]; then
+    # Enter key
+    case "$cursor" in
+      0)
+        echo "Creating and starting the container..."
+        docker-compose --env-file "$ENV_FILE" -f "$DOCKER_COMPOSE_FILE" up -d
+        read -p "Press any key to return to the menu..."
+        ;;
+      1)
+        echo "Stopping the container..."
+        docker-compose --env-file "$ENV_FILE" -f "$DOCKER_COMPOSE_FILE" stop
+        read -p "Press any key to return to the menu..."
+        ;;
+      2)
+        echo "Destroying and removing the container..."
+        docker-compose --env-file "$ENV_FILE" -f "$DOCKER_COMPOSE_FILE" down
+        read -p "Press any key to return to the menu..."
+        ;;
+      3)
+        echo "Exiting..."
+        exit 0
+        ;;
+    esac
+  fi
+}
+
+# Main loop to display the menu and handle input
 while true; do
   print_menu
-
-  # Read a single key press
-  read -rsn1 key
-
-  # Detect key codes for arrow keys
-  case "$key" in
-    $'\x1b')
-      read -rsn2 -t 0.1 key
-      if [[ $key == "[A" ]]; then
-        # Up arrow
-        ((cursor--))
-        if [ "$cursor" -lt 0 ]; then
-          cursor=$((${#options[@]} - 1))
-        fi
-      elif [[ $key == "[B" ]]; then
-        # Down arrow
-        ((cursor++))
-        if [ "$cursor" -ge "${#options[@]}" ]; then
-          cursor=0
-        fi
-      fi
-      ;;
-    "") # Enter key
-      case "$cursor" in
-        0)
-          echo "Creating and starting the container..."
-          docker-compose --env-file "$ENV_FILE" -f "$DOCKER_COMPOSE_FILE" up -d
-          read -p "Press any key to return to the menu..."
-          ;;
-        1)
-          echo "Stopping the container..."
-          docker-compose --env-file "$ENV_FILE" -f "$DOCKER_COMPOSE_FILE" stop
-          read -p "Press any key to return to the menu..."
-          ;;
-        2)
-          echo "Destroying and removing the container..."
-          docker-compose --env-file "$ENV_FILE" -f "$DOCKER_COMPOSE_FILE" down
-          read -p "Press any key to return to the menu..."
-          ;;
-        3)
-          echo "Exiting..."
-          break
-          ;;
-      esac
-      ;;
-  esac
+  move_cursor
 done
