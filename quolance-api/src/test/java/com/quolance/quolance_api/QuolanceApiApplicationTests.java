c8a216537483dd398166912cb9d45131fcc0153e
Sathurthikan Saththyvel <sathuow@gmail.com>
2024-11-06 14:51:14 -0500

    Reconfigured testing properties and updated branch

--- quolance-api/src/test/java/com/quolance/quolance_api/QuolanceApiApplicationTests.java
+++ quolance-api/src/test/java/com/quolance/quolance_api/QuolanceApiApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")  // This will make Spring use application-test.properties
+@ActiveProfiles("test")  // This will make Spring use application-test.yml
 class QuolanceApiApplicationTests {
 
     @Test
