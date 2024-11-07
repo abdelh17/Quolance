8303f660478621cb6d3edfcfd102c152e98ac3c8
ismail <ismail.feham64@gmail.com>
2024-11-07 02:04:17 -0500

    added freelancer react query hook #5 #64

---
+++ quolance-ui/src/models/freelancer/freelancer-hooks.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useMutation } from '@tanstack/react-query';
+import httpClient from '@/lib/httpClient';
+
+/*--- Hooks ---*/
+export const useSubmitApplication = () => {
+  return useMutation({
+    mutationFn: submitApplication,
+  });
+};
+
+/*--- Fetching functions ---*/
+// Hardcoded for now
+const submitApplication = async ({
+  freelancerId,
+  projectId,
+}: {
+  freelancerId: number;
+  projectId: number;
+  // we can a payload of specific type here in the future
+}) => {
+  return httpClient.post(`api/freelancer/submit-application`, {
+    freelancerId,
+    projectId,
+  });
+};
