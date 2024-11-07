0d760b9d04c9377c44e99ec2c29d6ae52d66cdf1
ismail <ismail.feham64@gmail.com>
2024-11-07 09:07:27 -0500

    Build fix #5 #64

--- quolance-ui/src/app/(with-main-layout)/(authenticated-pages)/(client-protected-pages)/my-projects/page.tsx
+++ quolance-ui/src/app/(with-main-layout)/(authenticated-pages)/(client-protected-pages)/my-projects/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import ClientProjectsView from '@/app/(with-main-layout)/(authenticated-pages)/(client-protected-pages)/my-projects/ClientProjectsView';
 
 function ClientProjectsPage() {
-  return <ClientProjectsPage />;
+  return <ClientProjectsView />;
 }
 
 export default ClientProjectsPage;
