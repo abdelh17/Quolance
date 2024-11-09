032ee6643c908c9aa6fbd7a4ae49e8f727660eb5
KA-DEVL <anes1999@live.ca>
2024-11-08 21:02:22 -0500

    Minor fix in HeaderSection

--- quolance-ui/src/components/home/HeroSection.tsx
+++ quolance-ui/src/components/home/HeroSection.tsx
@@ -7,12 +7,12 @@ import { PiPlusBold } from 'react-icons/pi';
 export default function HeroSection() {
   return (
     <div className='bg-white'>
-      <div className='relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14'>
+      <div className='relative isolate overflow-hidden bg-gradient-to-b '>
         <div
           aria-hidden='true'
-          className='absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96'
+          className='absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96'
         />
-        <div className='mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8'>
+        <div className='mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8'>
           <div className='mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-8 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8'>
             <h1 className='max-w-2xl text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto'>
               We’re changing the way You Find Contractors & Freelancers
