2a15e3e525b19a4b4bd3d6d39e797706dd4f1764
ismail <ismail.feham64@gmail.com>
2024-11-09 07:38:44 -0500

    Connected to backend with client-api #11 #59

--- quolance-ui/src/api/client-api.ts
+++ quolance-ui/src/api/client-api.ts
@@ -1,16 +1,25 @@
 import httpClient from '@/lib/httpClient';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 /*--- Hooks ---*/
 export const useGetProjectSubmissions = (projectId: number) => {
   return useQuery({
     queryKey: ['project-submissions', projectId], // Add projectId to queryKey, important for caching
     queryFn: () => getProjectSubmissions(projectId),
-    enabled: !!projectId,
+  });
+};
+
+export const useApproveSubmission = () => {
+  return useMutation({
+    mutationFn: approveSubmission,
   });
 };
 
 /*--- Query functions ---*/
 const getProjectSubmissions = async (projectId: number) => {
   return httpClient.get(`api/client/projects/${projectId}/applications`);
+};
+
+const approveSubmission = async (applicationId: number) => {
+  return httpClient.post(`api/client/${applicationId}/select-freelancer`);
 };
