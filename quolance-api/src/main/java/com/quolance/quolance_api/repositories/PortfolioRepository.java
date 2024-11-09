c9d30b400f3e9a299244eec311074bddd5d06616
Abdel <abdelkader.habel_pro@outlook.com>
2024-11-09 15:00:56 -0500

    removed unnecessary methods from portfolio repository #32

--- quolance-api/src/main/java/com/quolance/quolance_api/repositories/PortfolioRepository.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/repositories/PortfolioRepository.java
@@ -1,21 +1,13 @@
 package com.quolance.quolance_api.repositories;
 
 import com.quolance.quolance_api.entities.Portfolio;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-    
-    Portfolio findByName(String name);
-    
-    Portfolio save(Portfolio portfolio);
-
-    List<Portfolio> findAllByUserId(Long freelancerId);
 
     Optional<Portfolio> findByUserId(Long userId);
 }
