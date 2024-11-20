39eb6a8085e641b5d4b5ebe18ec87b03b4942d36
Itek01 <adelbouchatta@live.ca>
2024-11-20 17:05:47 -0500

    changed slight margin ui mistake

--- quolance-ui/src/app/(with-main-layout)/(unauthenticated-pages)/why-quolance/about-us/page.tsx
+++ quolance-ui/src/app/(with-main-layout)/(unauthenticated-pages)/why-quolance/about-us/page.tsx
@@ -18,9 +18,9 @@ const aboutUsData = [
   {
     question: "Who is behind Quolance?",
     answer: (
-      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto py-2">
         {/* Title */}
-        <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+        <div className="max-w-2xl mx-auto text-center">
           <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Meet the crew</h2>
           <p className="mt-1 text-gray-600 dark:text-neutral-400">Creative people</p>
         </div>
