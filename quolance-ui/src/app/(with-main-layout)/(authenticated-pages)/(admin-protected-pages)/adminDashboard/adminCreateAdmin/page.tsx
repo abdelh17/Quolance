614ac3f2ff4174b4454f3baf60720a445ddea1b8
francesco <francescoferrato@hotmail.com>
2024-11-22 20:50:29 -0500

    Updated ui for adminCreateAdmin page

--- quolance-ui/src/app/(with-main-layout)/(authenticated-pages)/(admin-protected-pages)/adminDashboard/adminCreateAdmin/page.tsx
+++ quolance-ui/src/app/(with-main-layout)/(authenticated-pages)/(admin-protected-pages)/adminDashboard/adminCreateAdmin/page.tsx
@@ -1,14 +1,15 @@
+
 import  {CreateAdminForm}  from "../../componentsAdmin/CreateAdminForm"
 
 
 export default function adminCreateAdmin(){
 
 
-    return(
-        
-        <>
-        <h1 className = "text-center  heading-1 m-10">Create Admin</h1>
-        <CreateAdminForm/>
-        </>
-    )
+   return(
+      
+        <div className="mt-4 md:mt-0 space-y-6 flex flex-col justify-center h-full min-w-52 max-w-screen-sm mx-auto pt-24 px-2">
+        <h1 className="text-2xl font-semibold">Create a new admin</h1>
+        <CreateAdminForm />
+      </div>
+   )
 }
