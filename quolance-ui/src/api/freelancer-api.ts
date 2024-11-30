845a94bc1f384c1ffb5c6a833e48f9a3ec0b8a6b
ismail <ismail.feham64@gmail.com>
2024-11-30 18:36:46 -0500

    Added endpoint for freelancers to cancel their application #133 #132

--- quolance-ui/src/api/freelancer-api.ts
+++ quolance-ui/src/api/freelancer-api.ts
@@ -1,30 +1,39 @@
 'use client';
 import { useMutation } from '@tanstack/react-query';
 import httpClient from '@/lib/httpClient';
-import { AxiosError } from 'axios';
+import { showToast } from '@/util/context/ToastProvider';
+import { HttpErrorResponse } from '@/constants/models/http/HttpErrorResponse';
 
 /*--- Hooks ---*/
-export const useSubmitApplication = (options?: {
-  onSuccess?: () => void;
-  onError?: (error: AxiosError) => void;
-}) => {
+export const useSubmitApplication = () => {
   return useMutation({
-    mutationFn: submitApplication,
-    onSuccess: options?.onSuccess,
-    onError: options?.onError,
+    mutationFn: (projectId: number) =>
+      httpClient.post(`api/freelancer/submit-application`, { projectId }),
+    onSuccess: () => {
+      showToast('Application submitted successfully', 'success');
+    },
+    onError: (error) => {
+      const ErrorResponse = error.response?.data as HttpErrorResponse;
+      showToast(
+        `Error submitting application: ${ErrorResponse.message}`,
+        'error'
+      );
+    },
+  });
+};
+
+export const useCancelApplication = () => {
+  return useMutation({
+    mutationFn: (applicationId: number) =>
+      httpClient.post(`api/freelancer/${applicationId}/cancel-application`),
+    onSuccess: () => {
+      showToast('Application cancelled successfully', 'success');
+    },
+    onError: (error) => {
+      const ErrorResponse = error.response?.data as HttpErrorResponse;
+      showToast(`Error: ${ErrorResponse.message}`, 'error');
+    },
   });
 };
 
 /*--- Query functions ---*/
-const submitApplication = async ({
-  freelancerId,
-  projectId,
-}: {
-  freelancerId: number;
-  projectId: number;
-}) => {
-  return httpClient.post(`api/freelancer/submit-application`, {
-    freelancerId,
-    projectId,
-  });
-};
