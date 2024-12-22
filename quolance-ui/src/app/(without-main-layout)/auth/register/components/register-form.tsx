7ec1e60b20764a08707dcd36106f1f8b66db44a0
ismail <ismail.feham64@gmail.com>
2024-12-22 02:23:41 -0500

    Reworked registration flow #176

--- quolance-ui/src/app/(without-main-layout)/auth/register/components/register-form.tsx
+++ quolance-ui/src/app/(without-main-layout)/auth/register/components/register-form.tsx
@@ -16,8 +16,14 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { HttpErrorResponse } from '@/constants/models/http/HttpErrorResponse';
 import { cn } from '@/util/utils';
+import { RegistrationUserType } from '@/app/(without-main-layout)/auth/register/page';
+import { Role } from '@/constants/models/user/UserResponse';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { getProviderLoginUrl } from '@/app/(without-main-layout)/auth/login/components/user-auth-form';
 
-type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement> & {
+  userRole: RegistrationUserType;
+};
 
 const registerSchema = z
   .object({
@@ -33,6 +39,21 @@ const registerSchema = z
     path: ['passwordConfirmation'],
   });
 
+const getPropsFromUserRole = (userRole: Role.CLIENT | Role.FREELANCER) => {
+  switch (userRole) {
+    case Role.CLIENT:
+      return {
+        title: 'Sign up as a client',
+        linkText: 'Apply as a freelancer',
+      };
+    case Role.FREELANCER:
+      return {
+        title: 'Sign up as a freelancer',
+        linkText: 'Join as a client',
+      };
+  }
+};
+
 type Schema = z.infer<typeof registerSchema>;
 export function UserRegisterForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
@@ -45,6 +66,8 @@ export function UserRegisterForm({ className, ...props }: UserAuthFormProps) {
     setErrors(undefined);
     setSuccess(false);
     setIsLoading(true);
+    // Add role to the data
+    data.role = props.userRole;
     httpClient
       .post('/api/users', data)
       .then(() => {
@@ -77,91 +100,137 @@ export function UserRegisterForm({ className, ...props }: UserAuthFormProps) {
           </Link>
         }
       />
+      <div className='flex flex-col gap-4 sm:flex-row'>
+        <Link href={getProviderLoginUrl('github')} className={'w-full'}>
+          <Button
+            variant='outline'
+            type='button'
+            disabled={isLoading}
+            className='w-full'
+          >
+            <FaGithub className='mr-2 h-4 w-4' />
+            GitHub
+          </Button>
+        </Link>
 
+        <Link href={getProviderLoginUrl('google')} className={'w-full'}>
+          <Button
+            variant='outline'
+            type='button'
+            disabled={isLoading}
+            className='w-full'
+          >
+            <FaGoogle className='mr-2 h-4 w-4' />
+            Google
+          </Button>
+        </Link>
+      </div>
+
+      <div className='relative my-2'>
+        <div className='absolute inset-0 flex items-center'>
+          <span className='w-full border-t' />
+        </div>
+        <div className='relative flex justify-center text-xs uppercase'>
+          <span className='text-muted-foreground bg-white px-2'>
+            Or continue with
+          </span>
+        </div>
+      </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className='grid gap-2'>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input
-              id='email'
-              placeholder='name@example.com'
-              type='text'
-              autoCapitalize='none'
-              autoComplete='email'
-              autoCorrect='off'
-              disabled={isLoading}
-              {...register('email')}
-            />
-            {formState.errors.email && (
-              <small className='text-red-600'>
-                {formState.errors.email.message}
-              </small>
-            )}
+          <div className='grid gap-3'>
+            <div className='grid grid-cols-2 gap-4'>
+              <div className='grid gap-2'>
+                <div>
+                  <Label htmlFor='firstName'>First name</Label>
+                  <Input
+                    id='firstName'
+                    type='text'
+                    autoCapitalize='none'
+                    autoCorrect='off'
+                    disabled={isLoading}
+                    {...register('firstName')}
+                  />
+                </div>
+              </div>
 
-            <Label htmlFor='password'>Password</Label>
-            <Input
-              id='password'
-              type='password'
-              autoCapitalize='none'
-              autoCorrect='off'
-              disabled={isLoading}
-              {...register('password')}
-            />
-            {formState.errors.password && (
-              <small className='text-red-600'>
-                {formState.errors.password.message}
-              </small>
-            )}
+              <div className='grid gap-2'>
+                <Label htmlFor='lastName'>Last name</Label>
+                <Input
+                  id='lastName'
+                  type='text'
+                  autoCapitalize='none'
+                  autoCorrect='off'
+                  disabled={isLoading}
+                  {...register('lastName')}
+                />
+              </div>
+            </div>
 
-            <Label htmlFor='passwordConfirmation'>Confirm password</Label>
-            <Input
-              id='passwordConfirmation'
-              type='password'
-              disabled={isLoading}
-              {...register('passwordConfirmation')}
-            />
-            {formState.errors.passwordConfirmation && (
-              <small className='text-red-600'>
-                {formState.errors.passwordConfirmation.message}
-              </small>
-            )}
+            <div>
+              <Label htmlFor='email'>Email</Label>
+              <Input
+                id='email'
+                placeholder='name@example.com'
+                type='text'
+                autoCapitalize='none'
+                autoComplete='email'
+                autoCorrect='off'
+                disabled={isLoading}
+                {...register('email')}
+              />
+              {formState.errors.email && (
+                <small className='text-red-600'>
+                  {formState.errors.email.message}
+                </small>
+              )}
+            </div>
 
-            <Label htmlFor='firstName'>First name</Label>
-            <Input
-              id='firstName'
-              type='text'
-              autoCapitalize='none'
-              autoCorrect='off'
-              disabled={isLoading}
-              {...register('firstName')}
-            />
+            <div>
+              <Label htmlFor='password'>Password</Label>
+              <Input
+                id='password'
+                type='password'
+                autoCapitalize='none'
+                autoCorrect='off'
+                disabled={isLoading}
+                {...register('password')}
+              />
+              {formState.errors.password && (
+                <small className='text-red-600'>
+                  {formState.errors.password.message}
+                </small>
+              )}
+            </div>
 
-            <Label htmlFor='lastName'>Last name</Label>
-            <Input
-              id='lastName'
-              type='text'
-              autoCapitalize='none'
-              autoCorrect='off'
-              disabled={isLoading}
-              {...register('lastName')}
-            />
-
-            <Label htmlFor='role'>Role</Label>
-            <select
-              id='role'
-              disabled={isLoading}
-              {...register('role')}
-              className='rounded border border-gray-300 p-2'
-            >
-              <option value=''>Select Role</option>
-              <option value='CLIENT'>CLIENT</option>
-              <option value='FREELANCER'>FREELANCER</option>
-            </select>
+            <div>
+              <Label htmlFor='passwordConfirmation'>Confirm password</Label>
+              <Input
+                id='passwordConfirmation'
+                type='password'
+                disabled={isLoading}
+                {...register('passwordConfirmation')}
+              />
+              {formState.errors.passwordConfirmation && (
+                <small className='text-red-600'>
+                  {formState.errors.passwordConfirmation.message}
+                </small>
+              )}
+            </div>
           </div>
 
           <ErrorFeedback data={errors} />
 
-          <Button disabled={isLoading} type='submit' variant={'footerColor'}>
+          <Button
+            className={'mt-6'}
+            disabled={isLoading}
+            animation={{
+              hoverTextColor: 'text-n700',
+              overlayColor: 'bg-yellow-400',
+            }}
+            bgColor={'n900'}
+            type='submit'
+          >
             {isLoading && 'Creating account...'}
             Register
           </Button>
