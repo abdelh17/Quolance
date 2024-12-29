25d81cd3b26fc77db1953484b56622a8b03835e3
Fadi <fadi_nimer@hotmail.com>
2024-12-29 03:01:59 -0500

    Fixed styling

--- quolance-ui/src/app/(with-main-layout)/(unauthenticated-pages)/whats-new/blog/page.tsx
+++ quolance-ui/src/app/(with-main-layout)/(unauthenticated-pages)/whats-new/blog/page.tsx
@@ -151,11 +151,11 @@ const blogPosts = [
 function BlogPage() {
   return (
     <>
-      <div className="bg-gray-800 dark:bg-gray-950">
+      <div className="bg-white">
         <div className="max-w-[1400px] mx-auto relative p-10 lg:p-16">
           <div className="max-w-2xl mb-10">
-              <h2 className="text-2xl font-bold md:text-4xl md:leading-tight text-gray-100">Blog</h2>
-              <p className="mt-1 text-gray-400 dark:text-neutral-400">
+              <h2 className="text-2xl font-bold md:text-4xl md:leading-tight text-gray-900">Blog</h2>
+              <p className="mt-1 text-gray-800">
                 Stay up to date with the latest news. Here you can find articles, events, news.
               </p>
           </div>
