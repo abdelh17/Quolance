3eb0bfd556ea9bde5d8f11b2b999b8ff11ff836b
KA-DEVL <anes1999@live.ca>
2024-12-30 21:25:40 -0500

    Fix navigation bar links and redirections #222

--- quolance-ui/src/components/global/Header.tsx
+++ quolance-ui/src/components/global/Header.tsx
@@ -3,13 +3,10 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import { PiCaretDown, PiListBold, PiPlusBold } from 'react-icons/pi';
-
 import { useAuthGuard } from '@/api/auth-api';
-
 import MobileMenu from './MobileMenu';
 import { headerMenu } from '@/constants/data';
 import useScroll from '@/util/hooks/useScroll';
-
 import {
   Disclosure,
   Menu,
@@ -41,8 +38,8 @@ function Header() {
     id: string;
     name: string;
     isSubmenu: boolean;
-    link?: string; // Link is optional for parent menu items
-    submenu?: SubMenuItem[]; // Submenu is an array of SubMenuItem objects
+    link?: string;
+    submenu?: SubMenuItem[];
   }
 
   interface SubMenuItem {
@@ -59,6 +56,7 @@ function Header() {
   };
 
   const { scrolled } = useScroll();
+
   return (
     <header className='h-24'>
       <div
@@ -70,156 +68,166 @@ function Header() {
       >
         <div className='max-xxl:container xxl:px-25'>
           <div className='text-s1 flex items-center justify-between py-6'>
-            <div className='flex items-center justify-start gap-3 pb-1'>
-              <button
-                className='4xl:hidden text-4xl !leading-none'
-                onClick={() => setShowMobileMenu(true)}
-              >
-                <PiListBold className='text-b500 pt-2' />
-              </button>
-              {
-                user?.role? (
-                    <Link href='/dashboard'>
-                        <h1 className='text-2xl font-bold'>Quolance</h1>
-                    </Link>
-                    ) : (
-                    <Link href='/'>
-                        <h1 className='text-2xl font-bold'>Quolance</h1>
-                    </Link>
-                )
-              }
+            {/* Left side: Logo and non-user related links */}
+            <div className='flex items-center gap-6'>
+              <div className='flex items-center justify-start gap-3 pb-1'>
+                <button
+                  className='4xl:hidden text-4xl !leading-none'
+                  onClick={() => setShowMobileMenu(true)}
+                >
+                  <PiListBold className='text-b500 pt-2' />
+                </button>
+                {user?.role ? (
+                  <Link href='/dashboard'>
+                    <h1 className='text-2xl font-bold'>Quolance</h1>
+                  </Link>
+                ) : (
+                  <Link href='/'>
+                    <h1 className='text-2xl font-bold'>Quolance</h1>
+                  </Link>
+                )}
+              </div>
+
+              <div className='max-4xl:hidden'>
+                <ul className='xxl:gap-6 flex items-center justify-start gap-2 font-medium max-sm:hidden'>
+                  {headerMenu.map((menu) => (
+                    <li key={menu.id}>
+                      {menu.isSubmenu && (
+                        <div className='group relative cursor-pointer'>
+                          <div
+                            className={`subMenuTitle hover:text-b500 relative flex items-center justify-center gap-1 px-2 py-3 ${
+                              isMenuActive(menu) ? 'text-b500' : ''
+                            }`}
+                          >
+                            {menu.name}
+                            <PiCaretDown className='block pt-0.5 duration-700 group-hover:rotate-180' />
+                          </div>
+                          <ul className='group-hover:eventunset ltl:left-0 bg-b300 pointer-events-none invisible absolute top-12 flex w-[220px] translate-y-8 scale-75 flex-col items-start justify-start gap-3 rounded-lg py-6 font-medium text-white/90 opacity-0 duration-500 group-hover:visible group-hover:z-50 group-hover:translate-y-0 group-hover:scale-100 group-hover:opacity-100 rtl:right-0'>
+                            {menu.submenu?.map(({ id, name, link }) => (
+                              <li key={id}>
+                                <Link
+                                  href={link}
+                                  className={`subMenuItem hover:text-y200 px-6 duration-500 hover:ml-2 ${
+                                    path === link && 'text-y200'
+                                  }`}
+                                >
+                                  {name}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
 
-            <div className='max-4xl:hidden flex items-center justify-between gap-6 max-lg:hidden'>
-              <ul className='xxl:gap-6 flex items-center justify-start gap-2 font-medium max-sm:hidden'>
-                {headerMenu.map((menu) => (
-                  <li key={menu.id}>
-                    {menu.isSubmenu && (
-                      <div className='group relative cursor-pointer'>
-                        <div
-                          className={`subMenuTitle hover:text-b500 relative flex items-center justify-center gap-1 px-2 py-3 ${
-                            isMenuActive(menu) ? 'text-b500' : ''
-                          }`}
-                        >
-                          {menu.name}
-                          <PiCaretDown className='block pt-0.5 duration-700 group-hover:rotate-180' />
-                        </div>
-                        <ul className='group-hover:eventunset ltl:left-0 bg-b300 pointer-events-none invisible absolute top-12 flex w-[220px] translate-y-8 scale-75 flex-col items-start justify-start gap-3 rounded-lg py-6 font-medium text-white/90 opacity-0 duration-500 group-hover:visible group-hover:z-50 group-hover:translate-y-0 group-hover:scale-100 group-hover:opacity-100 rtl:right-0'>
-                          {menu.submenu?.map(({ id, name, link }) => (
-                            <li key={id}>
-                              <Link
-                                href={link}
-                                className={`subMenuItem hover:text-y200 px-6 duration-500 hover:ml-2 ${
-                                  path === link && 'text-y200'
-                                }`}
-                              >
-                                {name}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
+            {/* Right side: User-related content */}
+            <div className='max-4xl:hidden flex items-center gap-6'>
+              {!user?.email ? (
+                <ul className='flex items-center gap-2 font-medium'>
+                  <li className='hover:text-b500 duration-500'>
+                    <Link
+                      href='/auth/register'
+                      className='rounded-lg px-2 py-3'
+                    >
+                      Sign up
+                    </Link>
                   </li>
-                ))}
-                {user?.email ? (
-                  <>
+                  <li className='hover:text-b500 duration-500'>
+                    <Link href='/auth/login' className='rounded-lg px-2 py-3'>
+                      Sign in
+                    </Link>
+                  </li>
+                </ul>
+              ) : (
+                <div className='flex items-center gap-6'>
+                  <ul className='xxl:gap-6 flex items-center justify-start gap-2 font-medium max-sm:hidden'>
                     <li className='hover:text-b500 duration-500'>
-                      <Link href='/profile'>Profile</Link>
+                      <Link href='/dashboard'>Dashboard</Link>
+                    </li>
+
+                    {user?.role === 'FREELANCER' && (
+                      <li className='hover:text-b500 duration-500'>
+                        <Link href='/profile'>My profile</Link>
+                      </li>
+                    )}
+                    <li className='hover:text-b500 duration-500'>
+                      <Link href='/setting'>Settings</Link>
                     </li>
                     <li
                       className='hover:text-b500 cursor-pointer duration-500'
                       onClick={() => logout()}
                     >
-                      Logout
+                      Sign out
                     </li>
-                  </>
-                ) : (
-                  <>
-                    <li className='hover:text-b500 duration-500'>
-                      <Link
-                        href='/auth/register'
-                        className='rounded-lg px-2 py-3 '
-                      >
-                        Sign up
-                      </Link>
-                    </li>
-                    <li className='hover:text-b500 duration-500'>
-                      <Link href='/auth/login' className='rounded-lg px-2 py-3'>
-                        Sign in
-                      </Link>
-                    </li>
-                  </>
-                )}
-              </ul>
-              {user && (
-                <Disclosure as='nav' className='bg-white'>
-                  <div className='flex items-center'>
-                    <button
-                      type='button'
-                      className='relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-                    >
-                      <span className='absolute -inset-1.5' />
-                      <span className='sr-only'>View notifications</span>
-                      <BellIcon aria-hidden='true' className='size-6' />
-                    </button>
+                  </ul>
 
-                    {/* Profile dropdown */}
-                    <Menu as='div' className='relative ml-3'>
-                      <div>
-                        <MenuButton className='relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
-                          <span className='absolute -inset-1.5' />
-                          <span className='sr-only'>Open user menu</span>
-                          <img
-                            alt=''
-                            src={dummyUser.imageUrl}
-                            className='size-8 rounded-full'
-                          />
-                        </MenuButton>
-                      </div>
-                      <MenuItems
-                        transition
-                        className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+                  <Disclosure as='nav' className='bg-white'>
+                    <div className='flex items-center'>
+                      <button
+                        type='button'
+                        className='relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                       >
-                        {userNavigation.map((item) => (
-                          <MenuItem key={item.name}>
-                            <Link
-                              href={item.href}
-                              className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none'
-                            >
-                              {item.name}
-                            </Link>
-                          </MenuItem>
-                        ))}
-                      </MenuItems>
-                    </Menu>
-                  </div>
-                </Disclosure>
+                        <span className='absolute -inset-1.5' />
+                        <span className='sr-only'>View notifications</span>
+                        <BellIcon aria-hidden='true' className='size-6' />
+                      </button>
+
+                      <Menu as='div' className='relative ml-3'>
+                        <div>
+                          <MenuButton className='relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+                            <span className='absolute -inset-1.5' />
+                            <span className='sr-only'>Open user menu</span>
+                            <img
+                              alt=''
+                              src={dummyUser.imageUrl}
+                              className='size-8 rounded-full'
+                            />
+                          </MenuButton>
+                        </div>
+                        <MenuItems
+                          transition
+                          className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+                        >
+                          {userNavigation.map((item) => (
+                            <MenuItem key={item.name}>
+                              <Link
+                                href={item.href}
+                                className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none'
+                              >
+                                {item.name}
+                              </Link>
+                            </MenuItem>
+                          ))}
+                        </MenuItems>
+                      </Menu>
+                    </div>
+                  </Disclosure>
+
+                  {user?.role === 'CLIENT' && (
+                    <Link
+                      href='/post-project'
+                      className='bg-b300 hover:text-n900 max-xxl:size-11 max-xxl:!leading-none xxl:px-8 xxl:py-3 relative flex items-center justify-center overflow-hidden rounded-full text-white duration-700 after:absolute after:inset-0 after:left-0 after:w-0 after:rounded-full after:bg-yellow-400 after:duration-700 hover:after:w-[calc(100%+2px)]'
+                    >
+                      <span className='max-xxl:hidden relative z-10'>
+                        Post A Project
+                      </span>
+                      <PiPlusBold className='xxl:hidden relative z-10 text-xl' />
+                    </Link>
+                  )}
+                  {user?.role === 'FREELANCER' && (
+                    <Link
+                      href='/projects'
+                      className='bg-b50 text-b300 hover:text-n900 relative overflow-hidden rounded-full px-8 py-3 duration-700 after:absolute after:inset-0 after:left-0 after:w-0 after:rounded-full after:bg-yellow-400 after:duration-700 hover:after:w-[calc(100%+2px)] max-xl:hidden'
+                    >
+                      <span className='relative z-10'>Browse All Projects</span>
+                    </Link>
+                  )}
+                </div>
               )}
-              <div className='flex items-center justify-between gap-3 font-semibold'>
-                {user?.role === 'CLIENT' && (
-                  <Link
-                    href='/post-project'
-                    className='bg-b300 hover:text-n900 max-xxl:size-11 max-xxl:!leading-none xxl:px-8 xxl:py-3 relative flex items-center justify-center overflow-hidden rounded-full text-white duration-700 after:absolute after:inset-0 after:left-0 after:w-0 after:rounded-full after:bg-yellow-400 after:duration-700 hover:after:w-[calc(100%+2px)]'
-                  >
-                    <span className='max-xxl:hidden relative z-10'>
-                      Post A Project
-                    </span>
-                    <PiPlusBold className=' xxl:hidden relative z-10 text-xl' />
-                  </Link>
-                )}
-                {user?.role === 'FREELANCER' && (
-                  <Link
-                    href=''
-                    className='bg-b50 text-b300 hover:text-n900 relative overflow-hidden rounded-full px-8 py-3 duration-700 after:absolute after:inset-0 after:left-0 after:w-0 after:rounded-full after:bg-yellow-400 after:duration-700 hover:after:w-[calc(100%+2px)] max-xl:hidden'
-                  >
-                    <span className='relative z-10 '>
-                      {' '}
-                      Offer Your Services{' '}
-                    </span>
-                  </Link>
-                )}
-              </div>
             </div>
           </div>
         </div>
--- quolance-ui/src/components/global/MobileMenu.tsx
+++ quolance-ui/src/components/global/MobileMenu.tsx
@@ -26,34 +26,40 @@ function MobileMenu({
           showMobileMenu
             ? 'translate-y-0  opacity-30 '
             : ' translate-y-[-100%] opacity-0 delay-500 '
-        } black/50 shadow-lg fixed top-0 z-[998] h-full w-full duration-700 4xl:hidden ltr:left-0 rtl:right-0`}
+        } black/50 4xl:hidden fixed top-0 z-[998] h-full w-full shadow-lg duration-700 ltr:left-0 rtl:right-0`}
         onClick={() => setShowMobileMenu(false)}
       ></div>
 
       <div
-        className={`bg-b300 fixed top-0 z-[999] flex h-full w-3/4 flex-col items-start justify-start gap-8 text-white/80 duration-700 min-[500px]:w-1/2 4xl:hidden lg:gap-20 ltr:left-0 rtl:right-0 ${
+        className={`bg-b300 4xl:hidden fixed top-0 z-[999] flex h-full w-3/4 flex-col items-start justify-start gap-8 text-white/80 duration-700 min-[500px]:w-1/2 lg:gap-20 ltr:left-0 rtl:right-0 ${
           showMobileMenu
             ? 'visible translate-y-0 opacity-100  delay-500'
             : ' invisible translate-y-[100%] opacity-50'
         }`}
       >
-        <div className='bg-white fixed top-0 flex w-full items-center justify-between p-4 sm:p-8 mt-2 mb-6'>
-          <Link href='/' className='text-black text-2xl font-bold'>
-            Quolance
-          </Link>
+        <div className=' top-0 mt-2 flex w-full items-center justify-between p-4 sm:p-8'>
+          {user ? (
+            <Link href='/dashboard' className='text-2xl font-bold text-white'>
+              Quolance
+            </Link>
+          ) : (
+            <Link href='/' className='text-2xl font-bold text-white'>
+              Quolance
+            </Link>
+          )}
           <div
             className='cursor-pointer !text-3xl '
             onClick={() => setShowMobileMenu(false)}
           >
-            <PiX color='black'/>
+            <PiX color='white' />
           </div>
         </div>
 
-        <ul className='flex w-full flex-col items-start gap-6 overflow-y-auto pb-10 pt-24 text-lg sm:text-xl lg:gap-10 ltr:pl-8 rtl:pr-8'>
+        <ul className='flex w-full flex-col items-start gap-6 overflow-y-auto pb-10 text-lg sm:text-xl lg:gap-10 ltr:pl-8 rtl:pr-8'>
           {headerMenu.map((menu) => (
             <li key={menu.id}>
               {menu.isSubmenu ? (
-                <div className='subMenuToggle group flex flex-col items-start justify-start mt-6'>
+                <div className='subMenuToggle group flex flex-col items-start justify-start'>
                   <div
                     className='flex cursor-pointer items-center justify-start'
                     onClick={() => handleToggle(menu.id)}
@@ -91,10 +97,18 @@ function MobileMenu({
             </li>
           ))}
 
-          {user?.email ? (
+          {user ? (
             <>
               <li className='hover:text-b500 duration-500'>
-                <Link href='/profile'>Profile</Link>
+                <Link href='/dashboard'>Dashboard</Link>
+              </li>
+              {user.role === 'FREELANCER' && (
+                <li className='hover:text-b500 duration-500'>
+                  <Link href='/profile'>My Profile</Link>
+                </li>
+              )}
+              <li className='hover:text-b500 duration-500'>
+                <Link href='/setting'>Settings</Link>
               </li>
               <li
                 className='hover:text-b500 cursor-pointer duration-500'
@@ -119,19 +133,23 @@ function MobileMenu({
           )}
         </ul>
 
-        <Link
-          href='/post-project'
-          className='bg-y300 text-n900 rounded-full px-4 py-2 text-base font-medium ltr:ml-4 rtl:mr-4'
-        >
-          Post A Project
-        </Link>
+        {user?.role === 'CLIENT' && (
+          <Link
+            href='/post-project'
+            className='bg-y300 text-n900 rounded-full px-4 py-2 text-base font-medium ltr:ml-4 rtl:mr-4'
+          >
+            Post A Project
+          </Link>
+        )}
 
-        <Link
-          href=''
-          className='mb-6 bg-y300 text-n900 rounded-full px-4 py-2 text-base font-medium ltr:ml-4 rtl:mr-4'
-        >
-          Offer Your Services
-        </Link>
+        {user?.role === 'FREELANCER' && (
+          <Link
+            href=''
+            className='bg-y300 text-n900 mb-6 rounded-full px-4 py-2 text-base font-medium ltr:ml-4 rtl:mr-4'
+          >
+            Browse All Projects
+          </Link>
+        )}
       </div>
     </nav>
   );
