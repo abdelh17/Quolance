3983a8f90c1ad41cea52b12d7284bbec84610cef
KA-DEVL <anes1999@live.ca>
2024-12-30 20:32:09 -0500

    Authenticated user cannot access to home page

--- quolance-ui/src/app/page.tsx
+++ quolance-ui/src/app/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
 import Header from '@/components/global/Header';
 import HeroSection from '@/components/home/HeroSection';
+import { useAuthGuard } from '@/api/auth-api';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const { user } = useAuthGuard({ middleware: 'auth' });
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push('/dashboard');
+    }
+  }, [user, router]);
+
   return (
     <main>
       <Header />
