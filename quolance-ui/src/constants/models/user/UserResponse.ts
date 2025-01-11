1322e06a55d0ac22e780d6b83cc5c21aad83a0e9
ismail <ismail.feham64@gmail.com>
2025-01-10 22:08:52 -0500

    Freelancer cards now show freelancer profile #226

--- quolance-ui/src/constants/models/user/UserResponse.ts
+++ quolance-ui/src/constants/models/user/UserResponse.ts
@@ -7,17 +7,34 @@ export interface UserResponse {
   verified: boolean;
   profileImageUrl?: string;
   connectedAccounts: ConnectedAccount[];
-  authorities: string[]
+  authorities: string[];
+}
+
+export interface FreelancerProfileType {
+  id: number;
+  userId: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  profileImageUrl: string;
+  bio: string;
+  contactEmail: string;
+  city: string;
+  state: string;
+  experienceLevel: string;
+  socialMediaLinks: string[];
+  skills: string[];
+  availability: string; // FULL_TIME, PART_TIME ...
 }
 
 interface ConnectedAccount {
-  provider: 'google' | 'github' | 'facebook' | 'okta' ;
+  provider: 'google' | 'github' | 'facebook' | 'okta';
   connectedAt: string;
 }
 
 export enum Role {
-  CLIENT = "CLIENT",
-  FREELANCER = "FREELANCER",
-  ADMIN = "ADMIN",
-  PENDING = "PENDING"
+  CLIENT = 'CLIENT',
+  FREELANCER = 'FREELANCER',
+  ADMIN = 'ADMIN',
+  PENDING = 'PENDING',
 }
