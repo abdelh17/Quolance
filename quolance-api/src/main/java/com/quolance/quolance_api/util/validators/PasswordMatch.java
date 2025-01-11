2460829474aac3ad6bb5dae64fd903d7d6dbae8b
Chems-Eddine Saidi <saidiceb@gmail.com>
2025-01-11 14:58:16 -0500

    #174 Fixed the issue regarding password matching and validation

--- quolance-api/src/main/java/com/quolance/quolance_api/util/validators/PasswordMatch.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/util/validators/PasswordMatch.java
@@ -1,13 +1,18 @@
 package com.quolance.quolance_api.util.validators;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 @Constraint(validatedBy = PasswordMatchValidator.class)
-@Target({java.lang.annotation.ElementType.TYPE})
-@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({TYPE})
+@Retention(RUNTIME)
 @Documented
 public @interface PasswordMatch {
 
@@ -15,8 +20,9 @@ public @interface PasswordMatch {
 
     Class<?>[] groups() default {};
 
-    Class<?>[] payload() default {};
+    Class<? extends Payload>[] payload() default {};
 
     String passwordField();
+
     String passwordConfirmationField();
 }
