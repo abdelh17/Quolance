6e1b17dcf2512b00175607d170989c844d391329
Abdel <abdelkader.habel_pro@outlook.com>
2025-01-12 18:17:46 -0500

    refactored blog post comments and fixed tests for #245

--- quolance-api/src/main/java/com/quolance/quolance_api/entities/BlogPost.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/entities/BlogPost.java
@@ -1,14 +1,14 @@
 package com.quolance.quolance_api.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "blog_posts")
@@ -19,10 +19,10 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @SuperBuilder
 public class BlogPost extends AbstractEntity {
 
-    @Column(nullable = true, length = 100)
+    @Column(length = 100)
     private String title;
 
-    @Column(nullable = false, length = 10000) // Ensures content has a size limit
+    @Column(nullable = false, length = 10000)
     private String content;
 
     @ManyToOne
@@ -30,13 +30,6 @@ public class BlogPost extends AbstractEntity {
     private User user;
 
     @OneToMany(mappedBy = "blogPost", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<BlogComment> blogComments = new ArrayList<>(); // Initialize as an empty list
-
-    // Placeholder relationships (null templates for now)
-//    @OneToMany(mappedBy = "blogPost", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Reaction> reactions = new ArrayList<>();
-
-//    @OneToMany(mappedBy = "blogPost", cascade = CascadeType.ALL, orphanRemoval = true)
-//    private List<Reply> replies = new ArrayList<>();
+    private List<BlogComment> blogComments = new ArrayList<>();
 
 }
