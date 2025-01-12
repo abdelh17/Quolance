4b1cca6b4621ca22779fd59dbcea504492d7c819
ismail <ismail.feham64@gmail.com>
2025-01-11 19:47:58 -0500

    Added util function to convert query to string #228

--- quolance-ui/src/util/stringUtils.ts
+++ quolance-ui/src/util/stringUtils.ts
@@ -1,3 +1,5 @@
+import { ProjectFilterQuery } from '@/api/projects-api';
+
 export const formatEnumString = (enumObj: string): string => {
   return enumObj
     .replace(/_/g, ' ')
@@ -32,4 +34,16 @@ export const formatPriceRange = (priceRange: string) => {
 
 export const formatPriceRangeNoDollar = (priceRange: string) => {
   return formatPriceRange(priceRange).replace(/\$/g, '');
+};
+
+export const queryToString = (
+  query: { [s: string]: unknown } | ArrayLike<unknown> | ProjectFilterQuery
+): string => {
+  const filteredQuery: Record<string, string> = Object.fromEntries(
+    Object.entries(query).filter(
+      ([_, value]) => value !== undefined && value !== ''
+    )
+  ) as Record<string, string>;
+
+  return new URLSearchParams(filteredQuery).toString();
 };
