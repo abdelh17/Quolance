a7a80dfd48e5cefc819a777fa8eff6b44695ab04
Fadi <fadi_nimer@hotmail.com>
2025-01-23 03:38:39 -0500

    Comment section design rework and display comments on demand
    - Reworked the general aesthetics of the comment section as it was too bulky and the gaps between comment cards would take away the feel of having continuous interactions.
    - made comment section appear only when Comments is clicked (but potentially display 3 comments by default)
    - added comment count display

--- quolance-ui/src/components/ui/blog/CommentCard.tsx
+++ quolance-ui/src/components/ui/blog/CommentCard.tsx
@@ -21,41 +21,42 @@ const CommentCard: React.FC<CommentCardProps> = ({
   const toggleExpand = () => setIsExpanded(!isExpanded);
 
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 mb-4">
-      <div className="flex items-start gap-4">
-        <Image
-          alt={`${authorName}'s profile`}
-          src={profilePicture || icon}
-          width={40}
-          height={40}
-          className="rounded-full object-cover"
-        />
-        <div className="flex-1">
-          <div className="flex justify-between items-center mb-2">
-            <p className="text-sm font-semibold text-gray-800">{authorName}</p>
-            <span className="text-xs text-gray-500">
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "numeric",
-              }).format(new Date(dateCreated))}
-            </span>
-          </div>
+    <div className="flex items-start gap-4 py-1.5">
+      {/* User Profile Picture */}
+      <Image
+        alt={`${authorName}'s profile`}
+        src={profilePicture || icon}
+        width={32}
+        height={32}
+        className="rounded-full object-cover"
+      />
 
-          <p className="text-sm text-gray-700">
-            {content.length > 150 && !isExpanded
-              ? `${content.substring(0, 150)}...`
-              : content}
-            {content.length > 150 && (
-              <button
-                onClick={toggleExpand}
-                className="text-blue-500 text-xs ml-1 hover:underline"
-              >
-                {isExpanded ? "Show less" : "Show more"}
-              </button>
-            )}
-          </p>
+      {/* Comment Content */}
+      <div className="flex-1">
+        <div className="flex justify-between items-center">
+          <p className="text-sm font-semibold text-gray-800">{authorName}</p>
+          <span className="text-xs text-gray-500">
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            }).format(new Date(dateCreated))}
+          </span>
         </div>
+
+        <p className="text-sm text-gray-700 mt-1">
+          {content.length > 150 && !isExpanded
+            ? `${content.substring(0, 150)}...`
+            : content}
+          {content.length > 150 && (
+            <button
+              onClick={toggleExpand}
+              className="text-blue-500 text-xs ml-1 hover:underline"
+            >
+              {isExpanded ? "Show less" : "Show more"}
+            </button>
+          )}
+        </p>
       </div>
     </div>
   );
--- quolance-ui/src/components/ui/blog/PostCard.tsx
+++ quolance-ui/src/components/ui/blog/PostCard.tsx
@@ -7,129 +7,138 @@ import CommentCard from "./CommentCard";
 import { CommentType } from "@/constants/types/blog-types";
 
 const mockComments = [
-    {
-      authorName: "John Doe",
-      profilePicture: "",
-      content: "This is an insightful blog post! I really enjoyed reading it.",
-      dateCreated: "2025-01-21T10:30:00Z",
-    },
-    {
-      authorName: "Jane Smith",
-      profilePicture: "",
-      content: "Thanks for sharing this information. It was very helpful!",
-      dateCreated: "2025-01-20T14:15:00Z",
-    },
-    {
-      authorName: "Alex Johnson",
-      profilePicture: "",
-      content: "I think there’s a lot to learn from this perspective. Great job!",
-      dateCreated: "2025-01-19T18:45:00Z",
-    },
-    {
-      authorName: "Emily Brown",
-      profilePicture: "",
-      content: "Loved this! Keep up the great work.",
-      dateCreated: "2025-01-18T08:20:00Z",
-    },
-    {
-      authorName: "Michael Green",
-      profilePicture: "",
-      content: "A really thought-provoking post. Thanks for sharing your thoughts!",
-      dateCreated: "2025-01-17T21:10:00Z",
-    },
+  {
+    authorName: "John Doe",
+    profilePicture: "",
+    content:
+      "This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it. This is an insightful blog post! I really enjoyed reading it.",
+    dateCreated: "2025-01-21T10:30:00Z",
+  },
+  {
+    authorName: "Jane Smith",
+    profilePicture: "",
+    content: "Thanks for sharing this information. It was very helpful!",
+    dateCreated: "2025-01-20T14:15:00Z",
+  },
+  {
+    authorName: "Alex Johnson",
+    profilePicture: "",
+    content: "I think there’s a lot to learn from this perspective. Great job!",
+    dateCreated: "2025-01-19T18:45:00Z",
+  },
+  {
+    authorName: "Emily Brown",
+    profilePicture: "",
+    content: "Loved this! Keep up the great work.",
+    dateCreated: "2025-01-18T08:20:00Z",
+  },
+  {
+    authorName: "Michael Green",
+    profilePicture: "",
+    content: "A really thought-provoking post. Thanks for sharing your thoughts!",
+    dateCreated: "2025-01-17T21:10:00Z",
+  },
 ];
-  
 
 interface PostCardProps {
-    id: number;
-    title: string;
-    content: string;
-    authorName: string;
-    dateCreated: string;
-    comments: CommentType[];
+  id: number;
+  title: string;
+  content: string;
+  authorName: string;
+  dateCreated: string;
+  comments: CommentType[];
 }
 
-const PostCard: React.FC<PostCardProps> = ({ id, title, content, authorName, dateCreated, comments }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
+const PostCard: React.FC<PostCardProps> = ({
+  id,
+  title,
+  content,
+  authorName,
+  dateCreated,
+  comments,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showComments, setShowComments] = useState(false);
 
-    const toggleExpand = () => setIsExpanded(!isExpanded);
+  const toggleExpand = () => setIsExpanded(!isExpanded);
+  const toggleComments = () => setShowComments(!showComments);
 
-    comments = mockComments;
+  comments = mockComments;
 
-    return (
-        <div className="bg-white shadow-md rounded-md">
-            {/* User Info */}
-            <div className="bg-slate-300 w-full rounded-t-md">
-                <div className="flex items-center mb-4 ml-5 py-5">
-                    <Image
-                        alt={`${authorName}'s profile`}
-                        src={icon} // profilePicture
-                        width={48}
-                        height={48}
-                        className="rounded-full object-cover"
-                    />
-                    <div className="ml-4">
-                        <p className="font-semibold text-gray-800">{authorName}</p>
-                        {/* <p className="text-sm text-gray-500">{fieldOfWork}</p> */}
-                    </div>
-                </div>
-            </div>
-
-            {/* Post Content */}
-            <div className="ml-5 mr-5 mb-5">
-                <div className="flex justify-between">
-                    <h3 className="text-md font-semibold text-gray-800">{title}</h3>
-                    <span className="text-sm text-gray-500">
-                        {new Intl.DateTimeFormat("en-US", {
-                            year: "numeric",
-                            month: "long",
-                            day: "numeric",
-                            }).format(new Date(dateCreated))}
-                    </span>
-                </div>
-                <div className={`mt-2 ${!isExpanded ? "line-clamp-3" : ""}`}>
-                    <p className="text-gray-700">{content}</p>
-                </div>
-
-                {content.length > 300 && (
-                    <button
-                    onClick={toggleExpand}
-                    className="text-blue-500 text-sm mt-2 focus:outline-none"
-                >
-                    {isExpanded ? "Read less" : "Read more"}
-                </button>
-                )}
-                
-                {/* Tags */}
-                {/* <div className="mt-4">
-                    <p className="text-sm font-semibold text-grey-800 mb-2">Tags: </p>
-                    {tags.map((tag, index) => (
-                        <span
-                            key={index}
-                            className="px-2 py-1 bg-gray-200 text-gray-700 rounded-full text-xs"
-                        >
-                            {tag}
-                        </span>
-                    ))}
-                </div> */}
-
-                {/* Comments */}
-                {(comments !== null && comments.length > 0) && (
-                    <div className="mt-4">
-                        {comments.map((comment, index) => (
-                            <CommentCard
-                                key={index}
-                                authorName={comment.authorName}
-                                profilePicture={comment.profilePicture || ''}
-                                content={comment.content}
-                                dateCreated={comment.dateCreated}
-                            />
-                        ))}
-                    </div>
-                )}
-            </div>
+  return (
+    <div className="bg-white shadow-md rounded-md">
+      {/* User Info */}
+      <div className="bg-slate-300 w-full rounded-t-md">
+        <div className="flex items-center mb-4 ml-5 py-5">
+          <Image
+            alt={`${authorName}'s profile`}
+            src={icon} // profilePicture
+            width={48}
+            height={48}
+            className="rounded-full object-cover"
+          />
+          <div className="ml-4">
+            <p className="font-semibold text-gray-800">{authorName}</p>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* Post Content */}
+      <div className="ml-5 mr-5 mb-5">
+        <div className="flex justify-between">
+          <h3 className="text-md font-semibold text-gray-800">{title}</h3>
+          <span className="text-sm text-gray-500">
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }).format(new Date(dateCreated))}
+          </span>
+        </div>
+        <div className={`mt-2 ${!isExpanded ? "line-clamp-3" : ""}`}>
+          <p className="text-gray-700">{content}</p>
+        </div>
+
+        {content.length > 300 && (
+          <button
+            onClick={toggleExpand}
+            className="text-blue-500 text-sm mt-2 focus:outline-none"
+          >
+            {isExpanded ? "Read less" : "Read more"}
+          </button>
+        )}
+
+        {/* Comments Toggle */}
+        <div className="mt-4">
+          <button
+            onClick={toggleComments}
+            className="text-blue-500 text-sm focus:outline-none"
+          >
+            Comments ({comments.length})
+          </button>
+        </div>
+
+        {/* Comments Section */}
+        {showComments && comments.length > 0 && (
+          <div className="bg-gray-50 p-4 rounded-md mt-2">
+            {comments.map((comment, index) => (
+              <React.Fragment key={index}>
+                <CommentCard
+                  authorName={comment.authorName}
+                  profilePicture={comment.profilePicture || ""}
+                  content={comment.content}
+                  dateCreated={comment.dateCreated}
+                />
+                {index < comments.length - 1 && (
+                  <hr className="border-t border-gray-200 my-0.5" />
+                )}
+              </React.Fragment>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
