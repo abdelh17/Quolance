e468e8df5f7e046c11325d81217f15132ef07811
francesco <francescoferrato@hotmail.com>
2025-01-23 21:52:32 -0500

    Added cypress test coverage for e2e tests

--- quolance-ui/cypress/support/e2e.ts
+++ quolance-ui/cypress/support/e2e.ts
@@ -13,5 +13,14 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+
 // Import commands.js using ES2015 syntax:
-import './commands'
+// import './commands'
+import '@testing-library/cypress/add-commands'
+
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+
+import '@cypress/code-coverage/support'
--- quolance-ui/src/app/layout.tsx
+++ quolance-ui/src/app/layout.tsx
@@ -1,44 +1,42 @@
 import type { Metadata } from 'next';
-import { Montserrat } from 'next/font/google';
+
 
 import './globals.css';
 
+
 import Footer from '@/components/global/Footer';
+
 
 import { StepsProvider } from '@/util/context/StepsContext';
 import ToastProvider from '@/util/context/ToastProvider';
 import ReactQueryProvider from '@/util/context/ReactQueryProvider';
 
-const monsterratFont = Montserrat({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-  variable: '--main-font',
-});
 
 export const metadata: Metadata = {
-  title: 'Quolance',
-  description: 'One Stop Freelancing Services',
+ title: 'Quolance',
+ description: 'One Stop Freelancing Services',
 };
 
+
 export default function RootLayout({
-  children,
+ children,
 }: Readonly<{
-  children: React.ReactNode;
+ children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en' dir='ltr'>
-      <body
-        className={`${monsterratFont.variable} ${monsterratFont.className} flex min-h-screen flex-col`}
-      >
-        <ToastProvider>
-          <ReactQueryProvider>
-            <StepsProvider>
-              <main className='flex-grow'>{children}</main>
-              <Footer />
-            </StepsProvider>
-          </ReactQueryProvider>
-        </ToastProvider>
-      </body>
-    </html>
-  );
+ return (
+   <html lang='en' dir='ltr'>
+     <body className='flex min-h-screen flex-col'>
+       <ToastProvider>
+         <ReactQueryProvider>
+           <StepsProvider>
+             <main className='flex-grow'>{children}</main>
+             <Footer />
+           </StepsProvider>
+         </ReactQueryProvider>
+       </ToastProvider>
+     </body>
+   </html>
+ );
 }
+
+
