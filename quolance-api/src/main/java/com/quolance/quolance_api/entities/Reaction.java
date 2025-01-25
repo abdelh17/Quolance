833485a8eb159d8e7b95d5d0a07b0a6ad464c707
Oussama.C <oussamacherifi7@gmail.com>
2025-01-25 00:00:10 -0500

    removed useless imports

--- quolance-api/src/main/java/com/quolance/quolance_api/entities/Reaction.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/entities/Reaction.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import com.quolance.quolance_api.entities.enums.ReactionTypeConstants;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
