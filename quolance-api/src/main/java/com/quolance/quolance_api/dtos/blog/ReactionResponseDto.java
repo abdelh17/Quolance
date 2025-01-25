9868c5a2e19abfce8d5d99f840fd8e24648e8eca
Oussama.C <oussamacherifi7@gmail.com>
2025-01-24 23:58:30 -0500

    Implemented responseDTO

---
+++ quolance-api/src/main/java/com/quolance/quolance_api/dtos/blog/ReactionResponseDto.java
@@ -0,0 +1,27 @@
+package com.quolance.quolance_api.dtos.blog;
+
+import com.quolance.quolance_api.entities.Reaction;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ReactionResponseDto {
+
+    private Long id;
+    private String userName;
+    private String reactionType;
+    private Long blogPostId;
+    private Long blogCommentId;
+
+    public static ReactionResponseDto fromEntity(Reaction reaction) {
+        return ReactionResponseDto.builder()
+                .id(reaction.getId())
+                .userName(reaction.getUser().getUsername())
+                .reactionType(reaction.getReactionType())
+                .blogPostId(reaction.getBlogPost() != null ? reaction.getBlogPost().getId() : null)
+                .blogCommentId(reaction.getBlogComment() != null ? reaction.getBlogComment().getId() : null)
+                .build();
+    }
+}
