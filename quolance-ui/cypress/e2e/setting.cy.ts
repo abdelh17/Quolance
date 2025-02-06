28c54e61a7047933fcc366a5f8cf65c0d47f6db3
francesco <francescoferrato@hotmail.com>
2025-02-06 13:47:36 -0500

    added back e2e tests to see if it triggers code coverage.

--- quolance-ui/cypress/e2e/setting.cy.ts
+++ quolance-ui/cypress/e2e/setting.cy.ts
@@ -1,7 +1,50 @@
 describe('Settings flow', () => {
 
 
-    it('Error Changing Password Flow', () => {
+
+
+  it('Error Changing Password Flow', () => {
+      // Mock api call for client authentication
+      cy.intercept('GET', '/api/auth/me', {
+        statusCode: 200,
+        body: {
+        firstName: 'John',
+        role: 'CLIENT',
+        verified: true,
+        },
+    }).as('getClientUser');
+     // Mock api error response for wrong current password
+    cy.intercept('PATCH', '/api/users/password', {
+      statusCode: 400,
+      body: {
+       message:"Wrong password"
+      },
+  }).as('currentPasswordError');
+      // Visit the login page
+      cy.visit("/setting");
+    
+     // Wait for the mocked API call
+    cy.wait('@getClientUser');
+
+     // Verify proper errors shown when submit button clicked with no inputs added to current password, new password, confirm password
+    cy.get('[data-test="change-password-btn"]').click();
+    cy.get('[data-test="old-password-error"]').contains(/Current password is required/i);
+    cy.get('[data-test="new-password-error"]').contains(/New password must be at least 8 characters long/i);
+    cy.get('[data-test="confirm-password-error"]').contains(/Password confirmation is required/i);
+      // Verify the passwords not matching error is displayed
+     cy.get('[data-test="old-password-input"]').type("Test1234!");
+    cy.get('[data-test="new-password-input"]').type("Password123");
+    cy.get('[data-test="confirm-password-input"]').type("Password123!");
+    cy.get('[data-test="change-password-btn"]').click();
+    cy.get('[data-test="confirm-password-error"]').contains(/Passwords do not match/i);
+     // Verify the wrong current password error is displayed
+    cy.get('[data-test="old-password-input"]').clear().type("Test1234");
+    cy.get('[data-test="confirm-password-input"]').clear().type("Password123");
+    cy.get('[data-test="change-password-btn"]').click();
+    cy.wait('@currentPasswordError');
+    cy.get('[data-test="old-password-error"]').contains(/Current password is incorrect/i);
+   });
+   it("No Error Changing Password Flow", () => {
         // Mock api call for client authentication
         cy.intercept('GET', '/api/auth/me', {
           statusCode: 200,
@@ -11,19 +54,26 @@ describe('Settings flow', () => {
           verified: true,
           },
       }).as('getClientUser');
-       // Mock api error response for wrong current password
+       // Mock api success response for changing current password
       cy.intercept('PATCH', '/api/users/password', {
-        statusCode: 400,
+        statusCode: 200,
         body: {
-         message:"Wrong password"
+        id:1,
+        role:"CLIENT",
+        firstName:"John"
         },
     }).as('currentPasswordError');
- 
- 
-        // Visit the login page
-        cy.visit("/setting");
-      
-     });
-    
+     // Visit the login page
+     cy.visit("/setting");
+     // Wait for the mocked API call
+    cy.wait('@getClientUser');
+    // Fill out inputs and submit
+   cy.get('[data-test="old-password-input"]').type("Test1234!");
+   cy.get('[data-test="new-password-input"]').type("Password123");
+   cy.get('[data-test="confirm-password-input"]').type("Password123");
+   cy.get('[data-test="change-password-btn"]').click();
+    // Verify the toast message for successful password change
+   cy.get('.Toastify__toast').should('be.visible').contains(/The password has been changed successfully/i);
    });
- 
+ });
+
