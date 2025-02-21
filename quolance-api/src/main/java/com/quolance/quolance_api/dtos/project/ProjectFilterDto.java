442fa7bdf3fcc9590abd8f264ce7b1cad509d169
SSathu <sathuow@gmail.com>
2025-02-20 22:36:34 -0500

    #358 Test Commit

--- quolance-api/src/main/java/com/quolance/quolance_api/dtos/project/ProjectFilterDto.java
+++ quolance-api/src/main/java/com/quolance/quolance_api/dtos/project/ProjectFilterDto.java
@@ -3,6 +3,7 @@ package com.quolance.quolance_api.dtos.project;
 import com.quolance.quolance_api.entities.enums.FreelancerExperienceLevel;
 import com.quolance.quolance_api.entities.enums.PriceRange;
 import com.quolance.quolance_api.entities.enums.ProjectCategory;
+import com.quolance.quolance_api.entities.enums.ProjectStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,4 +14,6 @@ public class ProjectFilterDto {
     private ProjectCategory category;
     private PriceRange priceRange;
     private FreelancerExperienceLevel experienceLevel;
+    private ProjectStatus projectStatus;
+
 }
