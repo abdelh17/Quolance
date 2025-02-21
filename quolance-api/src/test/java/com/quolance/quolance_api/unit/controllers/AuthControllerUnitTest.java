665fb17e9382d70ecfe8d039a30e526cdf881733
Abdel <abdelkader.habel_pro@outlook.com>
2025-02-20 20:22:01 -0500

    #395 updated commented out tests, toggle related

--- quolance-api/src/test/java/com/quolance/quolance_api/unit/controllers/AuthControllerUnitTest.java
+++ quolance-api/src/test/java/com/quolance/quolance_api/unit/controllers/AuthControllerUnitTest.java
@@ -21,7 +21,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.ArrayList;
 import java.util.UUID;
